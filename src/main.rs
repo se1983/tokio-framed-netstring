6c1425354ee11a9842bede850f88bb2b0f0d1887
sebastian <sebsch@geblubber.org>
2021-08-21 10:38:06 +0200

    improved algorithm

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,45 @@ use tokio::net::{TcpListener};
 
 #[allow(unused_imports)]
 use futures::sink::SinkExt;
-// enables next() on Framed
 use futures::StreamExt;
 use tokio::net::TcpStream;
 use tokio::io::AsyncWriteExt;
 
 
 // https://docs.rs/tokio-util/0.6.7/tokio_util/codec/index.html
+fn check(left: usize, right: usize, data: &[u8]) -> Result<(), Error> {
+    if let Ok(num_string) = String::from_utf8(Vec::from(&data[..left])){
+        if let Ok(exp_size) = num_string.parse::<usize>(){
+            if right > left && (right-1) - left == exp_size {
+                return Ok(())
+            }
+        }
+    }
+    Err(Error::new(ErrorKind::InvalidData, "Invalid data"))
+}
+
+
+fn extract_frameborders(src: &BytesMut) -> Option<(usize, usize)> {
+    let left_idx = src.iter().position(|&b| b == b':');
+    let right_idx = src.iter().position(|&b| b == b',');
+
+    match (left_idx, right_idx) {
+        (Some(l), Some(r)) => Some((l, r)),
+        _ => None
+    }
+}
 
 struct NetStringCodec {}
 
 impl NetStringCodec {
-
-
-    pub(crate) fn extract_frame(&self, delimiter_pos: usize, src: &mut BytesMut) -> Result<String, Box<dyn std::error::Error>> {
-
-        let length = &src[..delimiter_pos];
-        let expected_data_length = String::from_utf8(Vec::from(length)).unwrap().parse::<usize>().unwrap();
-        let rhs_pos = delimiter_pos + expected_data_length + 1;
-        if (src.len() as usize) < rhs_pos {
-            return Err("Incomplete".into());
+    pub(crate) fn extract_frame(&self, src: &mut BytesMut) -> Result<Option<String>, Box<dyn std::error::Error>> {
+        if let Some((lhs, rhs)) = extract_frameborders(src) {
+            check(lhs, rhs, src)?;
+            let data = src.split_to(rhs + 1); // <- modify src
+            let data = String::from_utf8(Vec::from(&data[lhs + 1..rhs]))?;
+            return Ok(Some(data));
         }
-        let data = src.split_to(rhs_pos); // <- modify src
-        Ok(String::from_utf8(Vec::from(&data[delimiter_pos + 1..])).unwrap())
+        Ok(None)
     }
 }
 
@@ -37,25 +53,22 @@ impl Decoder for NetStringCodec {
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         println!("Trying to decode {:?}", src);
-        if let Some(delimiter_idx) = src.iter().position(|&b| b == b':') {
-            let result = match self.extract_frame(delimiter_idx, src) {
-                Ok(frame) => Some(frame),
-                Err(err) if err.to_string() == "Incomplete" => { None }
-                _ => return Err(Error::new(
-                    ErrorKind::Other, "Something went wrong parsing data")
-                )
-            };
-            return Ok(result);
+
+        match self.extract_frame(src) {
+            Ok(frame) => Ok(frame),
+            Err(_) => Err(
+                Error::new(ErrorKind::InvalidData, "Could not parse data")
+            )
         }
-        Ok(None)
     }
 }
+
 
 impl Encoder<String> for NetStringCodec {
     type Error = std::io::Error;
 
     fn encode(&mut self, item: String, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let data = format!("{}:{}", item.len(), item);
+        let data = format!("{}:{},", item.len(), item);
         dst.put(data.as_bytes());
         Ok(())
     }
@@ -66,7 +79,6 @@ async fn server() {
         println!("listening with {:?}", tcp_listener);
         while let Ok((tcp_stream, sock_addr)) = tcp_listener.accept().await {
             println!("Accepted inbound from {}", sock_addr);
-
             tokio::spawn(async move {
                 let (_, mut reader) = Framed::new(tcp_stream, NetStringCodec {}).split();
                 while let Some(Ok(line)) = reader.next().await {
@@ -79,19 +91,16 @@ async fn server() {
 }
 
 
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-
-    let serv_handle = tokio::spawn({server()});
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let serv_handle = tokio::spawn(server());
 
     let mut stream = TcpStream::connect("127.0.0.1:7979").await?;
     // Write some data.
-    stream.write_all(b"5:hello").await?;
+    stream.write_all(b"5:hello,").await?;
+    stream.write_all(b"5:hello,").await?;
 
-    serv_handle.await;
+    let _ = serv_handle.await;
 
     Ok(())
-
-
 }
